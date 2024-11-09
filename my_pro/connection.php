f1d1bc5934ea33c3f34ee4418fe3db7be1b977f1
Channveer Mulge <130136525+c-mulge@users.noreply.github.com>
2024-11-09 09:51:39 +0530

    Delete my_pro directory

--- my_pro/connection.php
+++
@@ -1,9 +0,0 @@
-<?php 
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-    $con = mysqli_connect('localhost','root','','carproject');
-    if(!$con)
-    {
-        echo 'please check your Database connection';
-    }
-
-?>
